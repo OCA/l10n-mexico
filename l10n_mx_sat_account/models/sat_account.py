4191930994313f258781818779836d55acb755ff
Hiren Dangar <hiren.dangar.serpentcs@gmail.com>
2019-12-31 15:39:17 +0530

    [ADD]Added l10n_mx_sat_account module

---
+++ l10n_mx_sat_account/models/sat_account.py
@@ -0,0 +1,14 @@
+# Copyright (C) 2019 Open Source Integrators
+# Copyright (C) 2019 Serpent Consulting Services
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+from odoo import fields, models
+
+
+class SatAccount(models.Model):
+    _name = 'sat.account'
+
+    name = fields.Char(string='name')
+    code = fields.Char(string='Code')
+    parent_id = fields.Many2one(
+        comodel_name='sat.account',
+        string='Parent')
---
+++ l10n_mx_sat_account/readme/USAGE.rst
@@ -0,0 +1,11 @@
+Configuration
+
+* Go to Settings
+* Activate the developer mode
+* Go to Settings > Users
+* Select your user and make sure to check "Show Full Accounting Features"
+
+Usage
+
+* Go to Accounting > Configuration > Accounting > Chart of Accounts
+* For each account, set the field "SAT Grouping Account"
