c81b460a105693c62eb13bf683c64c75cd5ccea4
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2020-02-05 13:41:34 -0600

    [FIX] l10n_mx_sat_account

--- l10n_mx_sat_account/__manifest__.py
+++ l10n_mx_sat_account/__manifest__.py
@@ -2,25 +2,24 @@
 # Copyright (C) 2019 Serpent Consulting Services
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).lgpl.html).
 {
-    "name": "Maxico SAT Account",
-    "summary": """
-        This module provides the list of grouping accounts from
-        the Mexican tax authority, Servicio de Administración Tributaria (SAT):
-    """,
+    "name": "Mexico SAT Account",
+    "summary": "Provides the list of SAT grouping account to set on your CoA",
     "version": "13.0.1.0.0",
     "author": "Open Source Integrators, "
-    "Serpent Consulting Services Pvt. Ltd., "
-    "Odoo Community Association (OCA)",
+              "Serpent Consulting Services Pvt. Ltd., "
+              "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/operating-unit",
     "category": "Localization",
-    "depends": ["l10n_mx", "l10n_mx_sat_reference"],
+    "depends": [
+        "l10n_mx",
+        "l10n_mx_sat_reference"
+    ],
     "license": "AGPL-3",
     "data": [
         "security/ir.model.access.csv",
         "data/sat.account.csv",
         "views/sat_account.xml",
     ],
-    "installable": True,
     "development_status": "Beta",
     "maintainers": ["max3903"],
 }
--- l10n_mx_sat_account/readme/USAGE.rst
+++ l10n_mx_sat_account/readme/USAGE.rst
@@ -1,4 +1,2 @@
-Usage
-
 * Go to Accounting > Configuration > Accounting > Chart of Accounts
 * For each account, set the field "SAT Grouping Account"
