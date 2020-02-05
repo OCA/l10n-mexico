48198127e9a3423889ee7ef76da1f84438dc1a60
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2020-02-05 13:42:58 -0600

    [FIX] l10n_mx_sat_account

--- l10n_mx_sat_account/__manifest__.py
+++ l10n_mx_sat_account/__manifest__.py
@@ -3,17 +3,14 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).lgpl.html).
 {
     "name": "Mexico SAT Account",
-    "summary": "Provides the list of SAT grouping account to set on your CoA",
+    "summary": "Provides the list of SAT accounts to set on your CoA",
     "version": "13.0.1.0.0",
     "author": "Open Source Integrators, "
-              "Serpent Consulting Services Pvt. Ltd., "
-              "Odoo Community Association (OCA)",
+    "Serpent Consulting Services Pvt. Ltd., "
+    "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/operating-unit",
     "category": "Localization",
-    "depends": [
-        "l10n_mx",
-        "l10n_mx_sat_reference"
-    ],
+    "depends": ["l10n_mx", "l10n_mx_sat_reference"],
     "license": "AGPL-3",
     "data": [
         "security/ir.model.access.csv",
