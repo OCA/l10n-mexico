ef146f0f0d662634f1f81261ced131b0ae66b003
Murtuza Saleh <m.saleh.serpentcs@gmail.com>
2021-12-08 20:00:13 +0530

    [ADD] l10n_mx_pms

---
+++ l10n_mx_pms/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright (C) 2021 Open Source Integrators
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Property customizations for Mexico",
+    "summary": "Property customizations for Mexico",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "website": "https://github.com/OCA/l10n-mexico",
+    "author": "Open Source Integrators, " "Odoo Community Association (OCA)",
+    "category": "Localization",
+    "depends": ["pms_base", "base_address_extended"],
+    "development_status": "Beta",
+    "data": ["views/pms_property.xml"],
+    "maintainers": ["max3903"],
+}
