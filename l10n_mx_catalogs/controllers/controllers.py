754d696b62e63374ca2e174b3e24059873bf22e9
Alexis Lopez Zubieta <contact@azubieta.net>
2024-04-11 18:30:47 -0600

    Add SAT Catalogs

---
+++ l10n_mx_catalogs/controllers/controllers.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# from odoo import http
+
+
+# class MxSatCatalogs(http.Controller):
+#     @http.route('/mx_sat_catalogs/mx_sat_catalogs', auth='public')
+#     def index(self, **kw):
+#         return "Hello, world"
+
+#     @http.route('/mx_sat_catalogs/mx_sat_catalogs/objects', auth='public')
+#     def list(self, **kw):
+#         return http.request.render('mx_sat_catalogs.listing', {
+#             'root': '/mx_sat_catalogs/mx_sat_catalogs',
+#             'objects': http.request.env['mx_sat_catalogs.mx_sat_catalogs'].search([]),
+#         })
+
+#     @http.route('/mx_sat_catalogs/mx_sat_catalogs/objects/<model("mx_sat_catalogs.mx_sat_catalogs"):obj>', auth='public')
+#     def object(self, obj, **kw):
+#         return http.request.render('mx_sat_catalogs.object', {
+#             'object': obj
+#         })
---
+++ l10n_mx_catalogs/models/tipo_permiso.py
@@ -0,0 +1,11 @@
+from odoo import models, fields
+
+
+class TipoPermiso(models.Model):
+    _name = "l10n_mx_catalogs.c_tipo_permiso"
+    _description = "Catálogo Tipo de Permiso"
+    _rec_name = "description"
+
+    code = fields.Char(string="Código", required=True)
+    description = fields.Char(string="Descripción", required=True)
+    clave_transporte = fields.Char(string="Clave Transporte", required=True)
