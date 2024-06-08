86944c45cfcadf70ecf86aa396c7cbee4971f8db
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2024-06-08 15:24:19 -0600

    [FIX] l10n_mx_cfdi

---
+++ l10n_mx_cfdi/models/__init__.py
@@ -0,0 +1,10 @@
+from . import (
+    cfdi_series,
+    cfdi_service,
+    cfdi_issuer,
+    cfdi_document,
+    account_move,
+    account_tax,
+    account_payment,
+    account_payment_register,
+)
