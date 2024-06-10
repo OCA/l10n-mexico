39cf462951b9c7fc5036a8a5c58e05fa775630a7
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2024-06-09 22:08:55 -0600

    [FIX] l10n_mx_cfdi

--- l10n_mx_cfdi/models/__init__.py
+++ l10n_mx_cfdi/models/__init__.py
@@ -1,6 +1,7 @@
 from . import (
     cfdi_series,
     cfdi_service,
+    cfdi_service_topup,
     cfdi_issuer,
     cfdi_document,
     res_company,
---
+++ l10n_mx_cfdi/models/cfdi_service_topup.py
@@ -0,0 +1,48 @@
+from odoo import api, fields, models
+
+
+class CFDIServiceTopUp(models.Model):
+    _name = "l10n_mx_cfdi.cfdi_service.topup"
+    _description = "CFDI Service Top Up"
+
+    topup_date = fields.Datetime(
+        string="Fecha de Adquisición", default=fields.Datetime.now()
+    )
+    stamp_number = fields.Integer(string="Cantidad de Folios", required=True)
+    stamp_price = fields.Monetary(
+        string="Precio por Folio", required=True, currency_field="currency_id"
+    )
+    total = fields.Monetary(
+        string="Precio Total",
+        compute="_compute_total",
+        store=True,
+        currency_field="currency_id",
+    )
+
+    service_id = fields.Many2one(
+        "l10n_mx_cfdi.cfdi_service",
+        string="Servicio",
+        required=True,
+        ondelete="cascade",
+    )
+    partner_id = fields.Many2one(
+        "res.partner",
+        string="Contacto",
+        required=True,
+        readonly=True,
+        ondelete="restrict",
+        default=lambda self: self.env.user.partner_id,
+    )
+
+    currency_id = fields.Many2one(
+        "res.currency",
+        string="Moneda",
+        required=True,
+        readonly=True,
+        default=lambda self: self.env.company.currency_id,
+    )
+
+    @api.depends("stamp_number", "stamp_price")
+    def _compute_total(self):
+        for record in self:
+            record.total = record.stamp_number * record.stamp_price
