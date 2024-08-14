84091d3737628b882b73de12e90af9335a401a90
Santiago Rodriguez <santiromur@gmail.com>
2024-08-14 08:34:36 -0600

    [FIX] l10n_mx_cfdi: Tests init

--- l10n_mx_cfdi/tests/__init__.py
+++ l10n_mx_cfdi/tests/__init__.py
@@ -1,9 +1,3 @@
-# from . import test_account_move
-from . import test_account_move_line
-
-# from . import test_account_payment
-from . import test_account_tax
-
 # from . import test_cfdi_document
 from . import test_cfdi_issuer
 from . import test_cfdi_series
