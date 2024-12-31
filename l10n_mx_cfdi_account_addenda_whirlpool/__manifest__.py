61bfe545713215619b60729e28b6648a5fa48a2b
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2024-12-31 17:20:17 -0600

    [ADD] l10n_mx_edi_addenda_whirlpool

---
+++ l10n_mx_cfdi_account_addenda_whirlpool/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright (C) 2023 Open Source Integrators (https://www.opensourceintegrators.com)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+{
+    "name": "Mexican Addendum For Invoices For Whirlpool",
+    "version": "17.0.1.1.0",
+    "license": "LGPL-3",
+    "summary": "Mexican Localization Addendum For Whirlpool",
+    "author": "Open Source Integrators, Odoo Mexican Association (AMOdoo)",
+    "maintainer": "Open Source Integrators",
+    "website": "https://github.com/amxodoo/enterprise",
+    "depends": ["account", "l10n_mx_edi"],
+    "data": ["views/l10n_mx_addenda_whirlpool.xml"],
+    "application": False,
+    "maintainers": ["ursais"],
+}
