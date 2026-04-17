51fefc11c059e7ed2ffd791245013ee5caf7c274
Alexis Lopez Zubieta <alexis.lopez@augetec.com>
2026-04-17 12:47:29 -0600

    [fix] Improve error logging in CFDI service by formatting JSON output for better readability

--- l10n_mx_cfdi/models/cfdi_service.py
+++ l10n_mx_cfdi/models/cfdi_service.py
@@ -100,10 +100,11 @@ class CFDIService(models.Model):
                 _("No se creo el CFDI por el siguiente motivo:\n\n%s") % message
             )
 
-            _logger.warning("Unable to create CFDI:\n" + dumps(cfdi_data) + "\n" + dumps(e.error_json, indent=4))
+            _logger.warning(
+                "Unable to create CFDI:\n" + dumps(cfdi_data, indent=2) + "\n" + dumps(e.error_json, indent=2))
             raise UserError(error_message) from e
         except facturama.ApiError as e:
-            _logger.warning("Unable to create CFDI:\n" + dumps(cfdi_data) + "\n" + dumps(e, indent=4))
+            _logger.warning("Unable to create CFDI:\n" + dumps(cfdi_data, indent=2) + "\n" + dumps(e, indent=2))
             raise UserError(
                 _("Ocurrió un error con el servicio de facturación.\n")
             ) from e
