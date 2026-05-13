9d1fc61434a53f735ffa48a5e5d75989571eabed
Alexis Lopez Zubieta <alexis.lopez@augetec.com>
2026-05-12 21:27:19 -0600

    [FIX] __manifest__: Add 'sale' dependency and remove unused report

--- l10n_mx_cfdi_comex/__manifest__.py
+++ l10n_mx_cfdi_comex/__manifest__.py
@@ -8,10 +8,9 @@
     'category': 'Localization',
     'version': '19.0.0.0.1',
     'license': 'GPL-3',
-    'depends': ['base', 'account', 'stock_landed_costs', 'l10n_mx_cfdi_account', 'l10n_mx_catalogs_comex'],
+    'depends': ['base', 'account', 'sale', 'stock_landed_costs', 'l10n_mx_cfdi_account', 'l10n_mx_catalogs_comex'],
     'data': [
         'security/ir.model.access.csv',
-        'reports/report_invoice_document.xml',
         'views/pedimento_views.xml',
         'views/product_template_views.xml',
         'views/stock_landed_cost_views.xml',
