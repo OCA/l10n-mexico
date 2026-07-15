dc40fe31a736dad60842961f9f82e69d6a03a999
Maxime Chambreuil <maxime.chambreuil@graymatterlogic.com>
2026-07-15 11:50:34 -0600

    [MIG] l10n_mx_edi_addenda_mabe: Migration to 19.0
    Port from amxodoo/enterprise 17.0 onto OCA l10n-mexico, depending on
    l10n_mx_cfdi_account addenda framework from PR #103.

--- l10n_mx_cfdi_account_addenda_mabe/models/account_move.py
+++ l10n_mx_cfdi_account_addenda_mabe/models/account_move.py
@@ -1,14 +1,17 @@
+# Copyright (C) 2023 Open Source Integrators
+# (https://www.opensourceintegrators.com).
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
 from odoo import api, fields, models
 
 
 class AccountMove(models.Model):
     _inherit = "account.move"
 
-    # New Fields
     mabe_ref1 = fields.Char(string="Mabe Reference 1", default="NA")
     mabe_ref2 = fields.Char(string="Mabe Reference 2", default="NA")
     mabe_amount_with_letter = fields.Char(string="Amount with letter")
-    mabe_flag = fields.Boolean(default=False, compute="_compute_mabe_flag", store=True)
+    mabe_flag = fields.Boolean(compute="_compute_mabe_flag", store=True)
 
     @api.depends("partner_id.l10n_mx_edi_addenda_name")
     def _compute_mabe_flag(self):
---
+++ l10n_mx_cfdi_account_addenda_mabe/readme/DESCRIPTION.md
@@ -0,0 +1,2 @@
+This module adds the Mabe addenda for Mexican CFDI invoices and lets you
+generate the corresponding addenda XML when invoicing Mabe.
